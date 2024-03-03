541bf24bbc8f4d98c4d1de78ac841c5c2a02fc71
Yasemin <151784243+gelisimduygusu@users.noreply.github.com>
2024-03-03 23:40:52 +0000

    Create generateMarkdown.js

---
+++ generateMarkdown.js
@@ -0,0 +1,43 @@
+
+//  Generate markdown function
+function generateMarkdown(data) {
+  return `# ${data.title}  ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+ 
+  ## Description
+  ${data.description}
+  ## Table of Contents
+
+  <details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#Description">Description</a></li>
+    <li><a href="#Installation">Installation</a></li>
+    <li><a href="#Usage">Usage</a></li>
+    <li><a href="#Contributing">Contributing</a></li>
+    <li><a href="#Tests">Tests</a></li>
+    <li><a href="#Questions">Questions</a></li>
+    <li><a href="#License">License</a></li>
+  </ol>
+  </details>
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contribution}
+
+  ## Tests
+  ${data.test}
+
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at https://github.com/${data.username}
+  
+  ## License
+  This project is licensed under the ${data.license} license
+`;
+}
+
+module.exports = generateMarkdown;
